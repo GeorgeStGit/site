ffbff42feef36937c2a374b2fd7e83f293b75c65
George <georgstt@gmail.com>
2025-02-10 20:44:52 +1300

    update

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Serve HTML files for each route
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'home.html'));
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.get('/program', (req, res) => {
